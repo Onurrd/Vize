6b432c0540f6ce509dbfcf34cfff7aba8c26c9f1
Onur <onur.uresin@hotmail.com>
2023-11-12 17:39:45 +0300

    1.6 testi yapıldı.

--- 210401065/Labex1_Q1_210401065_onur_uresin.R
+++ 210401065/Labex1_Q1_210401065_onur_uresin.R
@@ -5,8 +5,11 @@ df <- read.csv("./MapsThatChangedOurWorld_StoryMap_Data.csv", sep = ";")
 maps <- data.frame(df)
 
 maps$Latitude<-gsub("N","",as.character(maps$Latitude))
+maps$Latitude<-as.numeric(maps$Latitude)
 
 idx <- grep("[W]",maps$Longitude)
 
 maps$Longitude<-gsub("E","",as.character(maps$Longitude))
 maps$Longitude<-gsub("W","",as.character(maps$Longitude))
+
+maps$Year<-gsub("AD","",as.character(maps$Year))
