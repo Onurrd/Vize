ccc6eb2b6eb0b4a56f57a16f615eff7d760a3845
Onur <onur.uresin@hotmail.com>
2023-11-12 17:20:54 +0300

    1.2 testi yapıldı.

--- 210401065/Labex1_Q1_210401065_onur_uresin.R
+++ 210401065/Labex1_Q1_210401065_onur_uresin.R
@@ -1,3 +1,7 @@
 download.file("https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv","MapsThatChangedOurWorld_StoryMap_Data.csv")
 
+df <- read.csv("./MapsThatChangedOurWorld_StoryMap_Data.csv", sep = ";")
+
+maps <- data.frame(df)
+
 
